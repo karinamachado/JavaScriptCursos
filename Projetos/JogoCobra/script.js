1659cfed4c97d46a047bd7e7e73ac4dcf6780b83
Karina Machado <karinamachado@MacBook-Pro-de-Karina.local>
2019-08-30 14:07:06 -0300

    git

--- Projetos/JogoCobra/script.js
+++ Projetos/JogoCobra/script.js
@@ -29,7 +29,22 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode === 37 && event.keyCode !== 'right') direction = 'left';
+    if(event.keyCode === 38 && event.keyCode !== 'down') direction = 'up';
+    if(event.keyCode === 39 && event.keyCode !== 'left') direction = 'right';
+    if(event.keyCode === 40 && event.keyCode !== 'up') direction = 'down';
+}
+
 function iniciarJogo(){
+    //se o valor de snake for maior que 15 e a direcao é pela direita, ela inicia na posicao 0 de X.
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBg();
     criarCobrinha();
 
@@ -55,6 +70,10 @@ function iniciarJogo(){
 // Passando o intervalo de 100 milisegundos para iniciar. Assim o jogo nao vai travar.
 
 let jogo = setInterval(iniciarJogo, 100);
+
+
+
+
 
 
 
